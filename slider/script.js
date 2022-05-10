9fb4a4d0eff321183c48d964770fc16ca26f1258
RaminQuliyev99 <98511186+RaminQuliyev99@users.noreply.github.com>
2022-05-10 10:42:34 +0400

    Add files via upload

---
+++ slider/script.js
@@ -0,0 +1,22 @@
+let sc = ["./honda-civic.jpg","./mitsubishi-lancer.jpg","./Toyota-supra.jpg"];
+let img = document.querySelector("#img");
+ 
+
+document.querySelector(".btn2").addEventListener(onclick=> {
+   img.src=$(sc.length);
+})
+
+
+// function ireli() {
+
+//     };
+
+// function geri() {
+//    sc.forEach(element => {
+//       img=src(element.length);
+//       element++
+//    })
+// }
+// sc.forEach(element => {
+//    img=src(element.length);
+//  element++
